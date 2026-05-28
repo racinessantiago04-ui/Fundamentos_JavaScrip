f8d5bdb6e223970db5045d3162e3accdc434271f
santiago <racinessantiago04@gmail.com>
2026-05-28 17:00:48 -0500

    Ejercicios_Java

---
+++ Cap1.introduccion/ex01/Ejercicio01.js
@@ -0,0 +1,2 @@
+document.write("Hola Mundo");// Esto se ve en el DOM
+console.log("Hola Mundo");// Esto se ve en la consola del navegador
---
+++ Cap1.introduccion/ex02/Ejercicio02.js
@@ -0,0 +1,32 @@
+var nombre = "Juan"; // Declaración de una variable llamada "nombre" y asignación del valor "Juan"
+console.log(nombre); // Imprime el valor de la variable "nombre" en la consola del navegador
+
+var nombre = "Pedro"; // Reasignación de la variable "nombre" con un nuevo valor "Pedro"
+console.log(nombre); // Imprime el nuevo valor de la variable "nombre" en la consola del navegador
+
+var nombre = "Maria"; // Reasignación de la variable "nombre" con un nuevo valor "Maria"
+console.log(nombre); // Imprime el nuevo valor de la variable "nombre" en la consola del navegador
+
+var nombre = "Ana"; // Reasignación de la variable "nombre" con un nuevo valor "Ana"
+console.log(nombre); // Imprime el nuevo valor de la variable "nombre" en la consola del navegador
+
+/*
+    Uso del la palabra reservada 'let' para declarar una variable llamada 'bebida' y asignarle diferentes valores a lo largo del código. Cada vez que se asigna un nuevo valor a la variable 'bebida', se muestra su contenido en la consola.
+
+    Diferencia entre var y let: La palabra reservada 'var' tiene un alcance de función, lo que significa que la variable declarada con 'var' es accesible dentro de la función en la que se declara. Por otro lado, 'let' tiene un alcance de bloque, lo que significa que la variable declarada con 'let' solo es accesible dentro del bloque en el que se declara (por ejemplo, dentro de un bucle o una estructura condicional). Además, 'let' no permite redeclarar la misma variable dentro del mismo bloque, mientras que 'var' sí lo permite.
+*/
+
+let bebida; // Declaración de la variable "bebida"
+bebida = "Coca-Cola"; // Asignación del valor "Coca-Cola" a la variable "bebida"
+console.log(bebida); // Imprime el valor de la variable "bebida" en la consola del navegador
+
+bebida = "Pepsi"; // Reasignación de la variable "bebida" con un nuevo valor "Pepsi"
+console.log(bebida); // Imprime el nuevo valor de la variable "bebida" en la consola del navegador
+
+bebida = "Fanta"; // Reasignación de la variable "bebida" con un nuevo valor "Fanta"
+console.log(bebida); // Imprime el nuevo valor de la variable "bebida" en la consola del navegador
+
+// uso de const para declarar una variable constante llamada "PI" y asignarle el valor de 3.1416. Las variables declaradas con const no pueden ser reasignadas, lo que significa que su valor no puede cambiar a lo largo del código.
+
+const PI = 3.1416; // Declaración de una variable constante llamada "PI" y asignación del valor 3.1416
+console.log(PI); // Imprime el valor de la variable constante "PI" en la consola del navegador
