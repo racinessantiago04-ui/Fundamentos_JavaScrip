597363f7c57b13361ba6a6a0de9ca20466fe3c30
santiago <racinessantiago04@gmail.com>
2026-06-04 17:07:08 -0500

    JavaScrip

---
+++ Cap1.introduccion/ex04/Ejercicio04.js
@@ -0,0 +1,19 @@
+let nombre, edad, direccion, telefono, email;
+
+nombre = prompt ("Ingrese su nombre:");
+edad = prompt ("Ingrese su edad:");
+direccion = prompt ("Ingrese su dirección:");
+telefono = prompt ("Ingrese su teléfono:");
+email = prompt ("Ingrese su correo electrónico:");
+
+console.log("Nombre: ", nombre);
+console.log("Edad: ", edad);
+console.log("Dirección: ", direccion);
+console.log("Teléfono: ", telefono);
+console.log("Correo electrónico: ", email);
+
+document.write("Tu nombre es: ", nombre, "<br>");
+document.write("Tu edad es: ", edad, "<br>");
+document.write("Tu dirección es: ", direccion, "<br>");
+document.write("Tu teléfono es: ", telefono, "<br>");
+document.write("Tu correo electrónico es: ", email, "<br>");
---
+++ Cap1.introduccion/ex05/Ejercicio05.js
@@ -0,0 +1,27 @@
+let a, b;
+
+let suma, resta, mult, div, residuo, potencia;
+
+a = parseInt(prompt ("ingresar el primer número:"));
+b = parseFloat(prompt ("ingresar el segundo número:"));
+
+
+// Resultados de operaciones
+suma = a + b;
+resta = a - b;
+mult = a * b;
+div = a / b;
+residuo = a % b;
+potencia = a ** b;
+
+// Imprimir resultados
+console.writeln("La suma es: ", suma);
+console.writeln("La resta es: ", resta);
+console.writeln("La multiplicación es: ", mult);
+console.writeln("La división es: ", div);
+console.writeln("El residuo es: ", residuo);
+console.writeln("La potencia es: ", potencia);
+
+
+
+
